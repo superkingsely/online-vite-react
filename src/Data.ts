c6f493fb6cca3876ded8f5daf4848b28507241e4
chijioke <superkingsely@gmail.com>
2024-09-09 11:03:55 -0700

    okay

--- src/Data.ts
+++ src/Data.ts
@@ -1,6 +1,7 @@
 import img from './../src/assets/slideimg.png'
 import cart from './assets/pexels-photo-2983464.jpeg'
-import product from './assets/pexels-photo-2983464.jpeg'
+import product1 from './assets/product-ecom1.jpg'
+import product2 from './assets/product-ecom2.jpg'
  export interface Islideobj{
     id:number,
     img:string,
@@ -8,7 +9,7 @@ import product from './assets/pexels-photo-2983464.jpeg'
     desc:string,
     bg:string
  }
-const isonline=true;
+const isonline=false;
 
 export const data:Array<Islideobj>=[
     {
@@ -66,42 +67,42 @@ export const data:Array<Islideobj>=[
   export const popularProducts = [
     {
       id:1,
-      img: `${isonline?'https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png':product}`,
+      img: `${isonline?'https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png':product1}`,
       
     },
     {
       id:2,
-      img: `${isonline?'https://www.google.com.ng/url?sa=i&url=https%3A%2F%2Fwww.reversible.com%2Fshopping%2Fmen%2Fitem%2Fprada-double-match-cotton-shirt-248803964&psig=AOvVaw00juU6Nfig8v-RoP8WMqOG&ust=1725990653917000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCLCA5bS2togDFQAAAAAdAAAAABAQ':product}`,
+      img: `${isonline?product2:product2}`,
       
     },
     {
       id:3,
-      img: `${isonline?'https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png':product}`,
+      img: `${isonline?product1:product1}`,
       
     },
     {
       id:4,
-      img: `${isonline?'https://www.burdastyle.com/pub/media/catalog/product/cache/7bd3727382ce0a860b68816435d76e26/107/BUS-PAT-BURTE-1320516/1170x1470_BS_2016_05_132_front.png':product}`,
+      img: `${isonline?'https://www.burdastyle.com/pub/media/catalog/product/cache/7bd3727382ce0a860b68816435d76e26/107/BUS-PAT-BURTE-1320516/1170x1470_BS_2016_05_132_front.png':product2}`,
       
     },
     {
       id:5,
-      img: `${isonline?'https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png':product}`,
+      img: `${isonline?'https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png':product1}`,
       
     },
     {
       id:6,
-      img: `${isonline?'https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png':product}`,
+      img: `${isonline?'https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png':product2}`,
       
     },
     {
       id:7,
-      img: `${isonline?'https://www.vintageindustries.nl/download_front/qympzk1762/2217_Arrow_Jacket_Forest.png':product}`,
+      img: `${isonline?product1:product1}`,
      
     },
     {
       id:8,
-      img: `${isonline?'https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png':product}`,
+      img: `${isonline?'https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png':product2}`,
       
     }
   ]
